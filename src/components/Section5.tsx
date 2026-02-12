67bd1a796708d1d51f53d7df62490be57ac4ba19
Vishwajeet Singh Parihar <svishwajeet750@gmail.com>
2026-02-12 02:08:46 +0000

    half section5

---
+++ src/components/Section5.tsx
@@ -0,0 +1,111 @@
+function Section5() {
+  return (
+    <section className="bg-bg-muted overflow-hidden">
+      <div className="container py-20 ">
+        <div className="mx-auto max-w-xl font-normal">
+          <h1 className="text-6xl font-medium ">Manual Controls</h1>
+          <div className="mt-6">
+            Physical buttons allow you to control the device manually without
+            connecting to PC or the mobile app. The large buttons are designed
+            for easy use without looking at the device.
+          </div>
+        </div>
+        <div className="mt-20 relative h-120   ">
+          <img
+            src="https://busy.bar/img/tild6136-3931-4564-b039-383730666233__busybar_manual_contr.png"
+            alt=""
+            className="absolute left-1/4 top-1/2 -translate-y-1/2  w-260"
+          />
+          <img
+            src="/Section5lines.svg"
+            alt=""
+            className="absolute opacity-25 w-240  hidden max-xl:block left-1/16 -top-1"
+          />{" "}
+          <div className="absolute  w-280 top-0 left-1/4 -translate-x-60 max-xl:hidden opacity-25">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="5412 4909.99951171875 1105.8876953125 415"
+              data-guides='{"vertical":[],"horizontal":[]}'
+            >
+              {" "}
+              <path
+                fill="transparent"
+                stroke="#000000"
+                fill-opacity="1"
+                stroke-width="1"
+                stroke-opacity="1"
+                id="tSvg8fa0df0c2c"
+                d="M 5416.8875765625 4956.999247412109L 5602.549388107316 4956.999247412109 A 12 12 0 0 1 5611.416333765486 4960.913625933518L 5678.0135765625 5033.946247412109"
+              ></path>{" "}
+              <path
+                fill="transparent"
+                stroke="#000000"
+                fill-opacity="1"
+                stroke-width="1"
+                stroke-opacity="1"
+                id="tSvgc0669a323"
+                d="M 5413.0000765625 5086.999747412109 C 5413.0000765625 5086.999747412109 5668.9998765625 5086.999747412109 5668.9998765625 5086.999747412109"
+              ></path>{" "}
+              <path
+                fill="transparent"
+                stroke="#000000"
+                fill-opacity="1"
+                stroke-width="1"
+                stroke-opacity="1"
+                id="tSvg2ec543d5ee"
+                d="M 5423.0001765625 5188.9104474121095L 5645.132295097345 5188.9104474121095 A 12 12 0 0 1 5653.617599064569 5192.425188630917L 5668.8813765625 5207.689047412109"
+              ></path>{" "}
+              <path
+                fill="transparent"
+                stroke="#000000"
+                fill-opacity="1"
+                stroke-width="1"
+                stroke-opacity="1"
+                id="tSvg4bb93f9a80"
+                d="M 5445.1028765625 5316.204747412109L 5625.455194955791 5316.204747412109 A 12 12 0 0 0 5634.534869119901 5312.050734735168L 5668.1029765625 5273.204447412109"
+              ></path>{" "}
+              <path
+                fill="transparent"
+                stroke="#000000"
+                fill-opacity="1"
+                stroke-width="1"
+                stroke-opacity="1"
+                id="tSvg183f782d5a5"
+                d="M 6016.1108765625 4978.999647412109 C 6016.1108765625 4978.999647412109 6016.1108765625 5078.999647412109 6016.1108765625 5078.999647412109"
+              ></path>{" "}
+              <path
+                fill="transparent"
+                stroke="#000000"
+                fill-opacity="1"
+                stroke-width="1"
+                stroke-opacity="1"
+                id="tSvg150f08dd4d2"
+                d="M 6400.8873765625 5047.999647412109L 6400.8873765625 4921.999647412109 A 11.000000000000002 11.000000000000002 0 0 1 6411.8873765625 4910.999647412109L 6422.8873765625 4910.999647412109"
+              ></path>{" "}
+              <path
+                fill="transparent"
+                stroke="#000000"
+                fill-opacity="1"
+                stroke-width="1"
+                stroke-opacity="1"
+                id="tSvg12d64172122"
+                d="M 5856.8875765625 5151.999647412109 C 5856.8875765625 5151.999647412109 5856.8875765625 5323.9994474121095 5856.8875765625 5323.9994474121095"
+              ></path>{" "}
+              <path
+                fill="transparent"
+                stroke="#000000"
+                fill-opacity="1"
+                stroke-width="1"
+                stroke-opacity="1"
+                id="tSvga2dd2ee111"
+                d="M 6516.8875765625 5195.999647412109 C 6516.8875765625 5195.999647412109 6516.8875765625 5323.9994474121095 6516.8875765625 5323.9994474121095"
+              ></path>{" "}
+              <defs></defs>{" "}
+            </svg>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Section5;
