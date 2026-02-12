642651b79930096d34ba86f394125a9e2c46cc48
Vishwajeet Singh Parihar <svishwajeet750@gmail.com>
2026-02-12 05:31:49 +0000

    section12

---
+++ src/components/Section12.tsx
@@ -0,0 +1,19 @@
+export default function Secion12() {
+  return (
+    <section className="h-dvh bg-[url(https://busy.bar/img/tild3738-3236-4435-b036-386131396163__busybar_smart_home_w.jpg)] bg-cover bg-center overflow-hidden">
+      <div className="z-0 flex flex-col gap-8 container   my-[10dvh] max-w-2xl">
+        <div className="text-6xl font-normal ">Smart home integration</div>
+        <div className="font-normal flex flex-col gap-4 max-w-[59ch] ">
+          <div>
+            Make your smart home react when you’re busy: pause music, turn on
+            lights, and lock the door with just one button.
+          </div>
+          <div className="">
+            BUSY Bar supports Matter®, a modern smart home protocol that
+            connects to Apple Home and Google Home.
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -10,6 +10,7 @@ import Section8 from "../components/Section8";
 import Section9 from "../components/Section9";
 import Section10 from "../components/Section10";
 import Section11 from "../components/Section11";
+import Section12 from "../components/Section12";
 
 export default function Landing() {
   return (
@@ -26,6 +27,7 @@ export default function Landing() {
       <Section9 />
       <Section10 />
       <Section11 />
+      <Section12 />
     </>
   );
 }
